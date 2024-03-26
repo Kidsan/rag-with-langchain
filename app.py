124bfb133ae1775432a07f72c09fc5f5cdd1fd73
kidsan <8798449+Kidsan@users.noreply.github.com>
2024-03-26 18:04:44 +0100

    feat(html): add html2text conversion

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
+from langchain_community import embeddings
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Qdrant
+from langchain_community.document_transformers import Html2TextTransformer
 from langchain.schema import Document
-
-# import qdrant_client
-# import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms import Ollama
-
-llm = Ollama(model="llama2")
+from qdrant_client import QdrantClient
+import os
 
 
 PROMPT_TEMPLATE = """
@@ -20,20 +19,20 @@ Answer the question based only on the following context:
 ---
 
 Answer the question based on the above context: {question}
+If the answer cannot be determined from the above context, just say you don't know.
 """
 
 
 def main():
+    llm = Ollama(model="llama2")
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     db = generate_data_store(False)
     while True:
         query_text = input(">>> ")
         found_docs = db.similarity_search_with_score(query_text)
-        context_text = f"\n\n---\n\n".join(
-            [doc.page_content for doc, _score in found_docs]
-        )
-        prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+        context_text = f"\n\n---\n\n".join([doc.page_content for doc, _ in found_docs])
         prompt = prompt_template.format(context=context_text, question=query_text)
-        print(prompt)
+        # print(prompt)
 
         res = llm.invoke(prompt)
         print(res)
@@ -41,11 +40,20 @@ def main():
 
 
 def generate_data_store(recreate: bool = False):
+    if os.path.isdir("./qdrant_data") or recreate == False:
+        client = QdrantClient(path="./qdrant_data/")
+        return Qdrant(
+            client=client,
+            collection_name="my_documents",
+            embeddings=OllamaEmbeddings(model="llama2:7b"),
+        )
     documents = load_documents("./docs/")
+    html2text = Html2TextTransformer()
+    documents = html2text.transform_documents(documents)
     chunks = split_text(documents)
     return Qdrant.from_documents(
         chunks,
-        OllamaEmbeddings(model="llama2:7b"),
+        OllamaEmbeddings(model="llama2:7b", show_progress=True),
         path="./qdrant_data",
         collection_name="my_documents",
         force_recreate=recreate,
@@ -65,14 +73,7 @@ def split_text(documents: list[Document]):
         length_function=len,
         add_start_index=True,
     )
-    chunks = text_splitter.split_documents(documents)
-    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
-
-    document = chunks[10]
-    print(document.page_content)
-    print(document.metadata)
-
-    return chunks
+    return text_splitter.split_documents(documents)
 
 
 if __name__ == "__main__":
